571d10f2b3fe4f55171be6dde5a3b6e6c2d124cc
prasanth <karanamprasanth.kp@gmail.com>
2026-05-01 18:30:14 +0530

    new change

--- variables.sh
+++ variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-read -t 5 -p "Enter value within 5 seconds: " val
-
-echo "Val: $val"
+echo "All args passed to scripts: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "who is the user: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this process: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to script: $*"
