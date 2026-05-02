65e498f7b00dd3a63ae412159e2ad1fc1cf1f8bd
prasanth <karanamprasanth.kp@gmail.com>
2026-05-02 11:15:48 +0530

    exitcode script

--- functions.sh
+++ functions.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+userid=(id -u)
+if [ userid -ne 0]; then
+    echo "User should be root install packages"
+    exit 1
+    else
+    echo "User has root privileges"
+fi
+
 validate(){
 if [ $1 -ne 0 ]; then
     echo "$2 is failure"
     exit 1
     else
-    echo "$2 is successfull"     
+    echo "$2 is successfull"    
+fi 
 }
 
 dnf install nodejs -y
