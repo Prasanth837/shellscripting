ceec4059de6654ec3dd0db182981f5355e6e8250
prasanth <karanamprasanth.kp@gmail.com>
2026-05-02 16:43:28 +0530

    exitcode script

---
+++ errorhandling.sh
@@ -0,0 +1,41 @@
+LOG_FOLDER="/var/log/shellscript"
+LOG_FILE="/var/log/shellscript/$0.log"
+R="\e[31m"  #Red colour
+G="\e[32m"  #Green Colour
+B="\e[34m"  #Blue Colour
+Y="\e[33m"  #Yellow Colour
+N="\e[0m"   #Normal black colour
+
+#we should start code with any colour but if we want to end the line with normal colour then give "\e[0m" for normal colour
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "$R User should be root install packages $N" | tee -a $LOG_FILE
+    exit 1
+    else
+    echo "$G User has root privileges $N" | tee -a $LOG_FILE
+fi
+
+mkdir -p $LOG_FOLDER
+
+validate(){
+if [ $1 -ne 0 ]; then
+    echo "$2 is failure" | tee -a $LOG_FILE
+    exit 1
+    else
+    echo "$2 is successfull" | tee -a $LOG_FILE
+fi 
+}
+
+for package in $@ #We pass data as arguements while executing this script example nginx mysql etc.
+do
+    dnf list installed $package &>> $LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, $Y Installing now $N "
+        dnf install $package -y &>> $LOG_FILE
+        validate $? "$package installation"
+    else
+        echo "$package already installed, $B skipping it $N "
+    fi
+done
