6bc81cf993f6c5d2a7cd08439163a5f0bbf8d3c2
prasanth <karanamprasanth.kp@gmail.com>
2026-05-02 11:40:46 +0530

    exitcode script

---
+++ logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/shellscript"
+LOG_FILE="/var/log/shellscript/$0.file"
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "User should be root install packages"
+    exit 1
+    else
+    echo "User has root privileges"
+fi
+
+mkdir -p $LOG_FOLDER
+
+validate(){
+if [ $1 -ne 0 ]; then
+    echo "$2 is failure"
+    exit 1
+    else
+    echo "$2 is successfull"    
+fi 
+}
+
+dnf install nodejs -y & >> $LOG_FILE
+validate $? "Installation of Nodejs"
+
+dnf install mysql & >> $LOG_FILE
+validate $? "Installation of mysql"
+
+dnf install nginx & >> $LOG_FILE
+validate $? "Installation of nginx"
