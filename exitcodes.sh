8b0ffb7cc8666018590517beb07897a3cdad2390
prasanth <karanamprasanth.kp@gmail.com>
2026-05-02 10:31:11 +0530

    exitcode script

--- exitcodes.sh
+++ exitcodes.sh
@@ -3,15 +3,16 @@
 User=$(id -u)
 
 if [ $User -nq 0 ]; then
-echo "Used id has no root permissions"
-exit 1
+    echo "Used id has no root permissions"
+    exit 1
 fi
 
 echo "Installing nginx service"
 dnf install nginx -y
 
-if [ $! -nq 0]; then
-echo "Installation of Nginx is unsuccessfull"
-exit 1
+if [ $! -nq 0 ]; then
+    echo "Installation of Nginx is unsuccessfull"
+    exit 1
 else
 echo "Installation of Nginx is successfull"
+fi
