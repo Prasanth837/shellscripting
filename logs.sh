f09b9a134a1989eea52c5cc5cce55e9ce0af510d
prasanth <karanamprasanth.kp@gmail.com>
2026-05-02 12:21:24 +0530

    exitcode script

--- logs.sh
+++ logs.sh
@@ -6,20 +6,20 @@ LOG_FILE="/var/log/shellscript/$0.log"
 userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
-    echo "User should be root install packages"
+    echo "User should be root install packages" | tee -a $LOG_FILE
     exit 1
     else
-    echo "User has root privileges"
+    echo "User has root privileges" | tee -a $LOG_FILE
 fi
 
 mkdir -p $LOG_FOLDER
 
 validate(){
 if [ $1 -ne 0 ]; then
-    echo "$2 is failure"
+    echo "$2 is failure" | tee -a $LOG_FILE
     exit 1
     else
-    echo "$2 is successfull"    
+    echo "$2 is successfull" | tee -a $LOG_FILE
 fi 
 }
 
