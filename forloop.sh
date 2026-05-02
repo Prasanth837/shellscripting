1d994b8dd03de65e370403673c4c6f8aa6b9313a
prasanth <karanamprasanth.kp@gmail.com>
2026-05-02 15:13:00 +0530

    exitcode script

---
+++ forloop.sh
@@ -0,0 +1,28 @@
+LOG_FOLDER="/var/log/shellscript"
+LOG_FILE="/var/log/shellscript/$0.log"
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "User should be root install packages" | tee -a $LOG_FILE
+    exit 1
+    else
+    echo "User has root privileges" | tee -a $LOG_FILE
+fi
+
+mkdir -p $LOG_FOLDER
+
+validate(){
+if [ $1 -ne 0 ]; then
+    echo "$2 is failure" | tee -a $LOG_FILE
+    exit 1
+    else
+    echo "$2 is successfull" | tee -a $LOG_FILE
+fi 
+}
+
+for package in $@ #We pass data as arguements while executing this script example nginx mysql etc.
+do
+dnf install $package -y &>> $LOG_FILE
+validate $? "$package installation"
+done
